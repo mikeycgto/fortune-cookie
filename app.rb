1b375c2dd7e9847fbaff468942838f0311497394
mikeycgto <mikeycgto@gmail.com>
2013-01-12 14:24:07 -0500

    Fix fortunes scope and add CORS support

--- app.rb
+++ app.rb
@@ -3,11 +3,13 @@ Bundler.require
 
 configure do
   set :fortune_file, File.expand_path('../fortunes', __FILE__)
+
+  @@fortunes = File.read(settings.fortune_file).split('%').map!(&:strip!)
 end
 
 helpers do
   def fortunes
-    @fortunes ||= File.read(settings.fortune_file).split('%').map!(&:strip!)
+    @@fortunes
   end
 
   def random_fortune
@@ -19,10 +21,21 @@ helpers do
   end
 end
 
-get '/' do
-  response['Set-Cookie'] = escape(random_fortune)
+before do
+  response['Access-Control-Allow-Origin'] = '*'
+  response['Access-Control-Allow-Method'] = 'GET'
+end
 
-  status 204
+get '/' do
+  fortune = random_fortune
+
+  response['Set-Cookie'] = escape(fortune)
+
+  if request.env['HTTP_ORIGIN']
+    halt 200, fortune
+  else
+    halt 204
+  end
 end
 
 get '*' do
